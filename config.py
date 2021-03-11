8474994d5e1e935a70e967cbe0a6e35ce155cd67
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2021-03-11 17:16:06 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import torch 
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-
-
-
-
-
+# Defining device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# Defining tokenizer
+tokenizer = T5Tokenizer.from_pretrained("t5-base")
+# Defining model
+model = T5ForConditionalGeneration.from_pretrained("t5-base")
